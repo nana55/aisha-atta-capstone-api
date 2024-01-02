2bcea56b2f3a971718bf79b2e1d327fe503e1693
Aisha Atta <aisha.atta1@gmail.com>
2024-01-02 00:47:56 -0800

    check for token verification to view goals

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,9 +1,19 @@
 const knex = require('knex')(require('../knexfile'));
+const jwt = require('jsonwebtoken');
 
 const index = async (req, res) => {
+    const token = req.cookies.accessToken;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized - No token provided' });
+    }
+
     try {
+        //Verify JWT token
+        const decoded = jwt.verify(token, 'secretkey');
+        console.log("Received Token:", decoded);
         const data = await knex('goals')
-        .select('goals.*', 'users.name as userName', 'users.avatar')
+            .select('goals.*', 'users.name as userName', 'users.avatar')
             .join('users', 'goals.user_id', 'users.id');
         res.status(200).json(data);
     } catch (err) {
