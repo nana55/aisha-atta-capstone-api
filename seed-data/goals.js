52f2080c340527c5ee5a747d3be02d51c3a34f9c
Aisha Atta <aisha.atta1@gmail.com>
2024-01-07 23:42:52 -0800

    add image links to seed data

--- seed-data/goals.js
+++ seed-data/goals.js
@@ -2,25 +2,25 @@ module.exports =
 [
     {
       description: 'Explore nature every weekend',
-      image: 'https://example.com/nature.jpg',
+      image: 'https://hips.hearstapps.com/hmg-prod/images/nature-quotes-landscape-1648265299.jpg',
       user_id: 1,
       category: 'physical-health',
     },
     {
       description: 'Read a book each month',
-      image: 'https://example.com/book.jpg',
+      image: 'https://media.cntraveller.com/photos/611bf0b8f6bd8f17556db5e4/master/pass/gettyimages-1146431497.jpg',
       user_id: 2,
       category: 'mental-health',
     },
     {
       description: 'Complete a coding project',
-      image: 'https://example.com/code.jpg',
+      image: 'https://assets-global.website-files.com/648f76da04dfc69f8db5bb19/64e6da8edda885b595a42c30_how-to-get-your-study-motivation-ignited.png',
       user_id: 3,
       category: 'work-and-productivity',
     },
     {
       description: 'Have a weekly date night',
-      image: 'https://example.com/date.jpg',
+      image: 'https://i.scdn.co/image/ab67616d0000b273276d8a2d0264506d16e5eb96',
       user_id: 4,
       category: 'relationships',
     },
